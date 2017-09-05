c3081b799205dbedf36fb2bf8946ba53ec3dc3e7
robimc14 <matthew.robinson@yale.edu>
2017-09-05 15:39:03 -0400

    updated argparse

--- run_chain_joiner_online.py
+++ run_chain_joiner_online.py
@@ -16,49 +16,13 @@ import sys
 import subprocess
 import argparse
 
-def main():
+import chain_joiner
 
-    #create parser object
-    parser = argparse.ArgumentParser(
-        prog='run_chain_joiner_online.py',
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        description="""
-    SCRIPT TO FIX MISSING RESIDUES IN PDB FILES.
-    This version obtains the PDB and FASTA files 
-    for the user directly from the web.
+def main(pdb_id, a, fm, l):
 
-    @author: Matthew Robinson matthew.robinson@yale.edu
-    @author: William L. Jorgensen Lab 
-    
-    REQUIREMENTS:
-    Preferably Anaconda python 3 environment with following modules:
-
-    Modeller
-    pandas 
-    biopandas
-    argparse
-    getopt
-    numpy
-    """)
-
-    #defining arguments for parser object
-    parser.add_argument('pdb_id')
-    parser.add_argument(
-        "-a", "--automodel", help="The simplest method for simple comparitive modeling.")
-    parser.add_argument(
-        "-l", "--loopmodel", help="Builds a model by refining the loop with the missing residues.")
-    parser.add_argument(
-        "-f", "--fixed_automodel", help="Builds an automodel and keeps the non-missing residues fixed.")
-
-    #parse the arguments from standard input
-    args = parser.parse_args()
-
-    #pdb_id = sys.argv[1]
-    pdb_id = str(args.pdb_id)
-
-    #make url's for wget
+    # make url's for wget
     pdb_url = 'https://files.rcsb.org/download/' + pdb_id + '.pdb'
-    #fasta_url = 'http://www.rcsb.org/pdb/download/viewFastaFiles.do?structureIdList=' + pdb_id + '&compressionType=uncompressed'
+    # fasta_url = 'http://www.rcsb.org/pdb/download/viewFastaFiles.do?structureIdList=' + pdb_id + '&compressionType=uncompressed'
     fasta_url = 'http://www.rcsb.org/pdb/download/viewFastaFiles.do?structureIdList=' + pdb_id
 
     #make file names
@@ -71,36 +35,64 @@ def main():
     #os.system('wget -q -O - ' + fasta_url + ' | gzip -d > ' + fasta_fn)
     #os.system('gzip -d ' + fasta_fn + '.gz')
 
-    #call the functions based on flag
-    if args.automodel:
-        subprocess.call(['python', 'chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-a'])
+    # run chain joiner
+    chain_joiner.main(pdb_fn, fasta_fn, a, fm, l )
 
-    if args.loopmodel:
-        subprocess.call(['python', 'chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-l'])
+    # # make a folder for the output
+    # dir_name = './' + pdb_id +'_output/'
+    # os.makedirs(dir_name)
 
-    elif args.fixed_automodel:
-        subprocess.call(['python', 'chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-f'])
+    # # get a list of all output files in the working directory
+    # output_files = [filename for filename in os.listdir('.') if filename.startswith(pdb_id)]
+    # # remove the folder name
+    # if (pdb_id + '_output') in output_files:
+    #     output_files.remove(pdb_id + '_output')
 
-    else:
-        subprocess.call(['python', 'chain_joiner.py', pdb_id + '.pdb', pdb_id + '.fasta','-a'])
-
-    #make a folder for the output
-    dir_name = './' + pdb_id +'_output/'
-    os.makedirs(dir_name)
-
-    #get a list of all output files in the working directory
-    output_files = [filename for filename in os.listdir('.') if filename.startswith(pdb_id)]
-    #remove the folder name
-    if (pdb_id + '_output') in output_files:
-        output_files.remove(pdb_id + '_output')
-
-    #mv these files to the output folder
-    for file in output_files:
-        try:
-            os.system('mv ' + file + ' ./' + pdb_id + '_output/')   
-        except:
-            pass
+    # # mv these files to the output folder
+    # for file in output_files:
+    #     try:
+    #         os.system('mv ' + file + ' ./' + pdb_id + '_output/')   
+    #     except:
+    #         pass
 
 if __name__ == "__main__":
-    # calling the main function
-    main()
+
+    #create parser object
+    parser = argparse.ArgumentParser(
+        prog='run_chain_joiner_online.py',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        description=
+    """
+    SCRIPT TO FIX MISSING RESIDUES IN PDB FILES.
+    This version obtains the PDB and FASTA files 
+    for the user directly from the PDB website.
+
+    @author: Matthew Robinson, matthew.robinson@yale.edu
+    @author: William L. Jorgensen Lab, Yale University 
+
+    Usage: python run_chain_joiner_online.py -p 1qg8 -a
+    
+    REQUIREMENTS:
+    Preferably Anaconda python 3 environment with following modules:    
+        Modeller
+        pandas 
+        biopandas
+        argparse
+    """
+    )
+
+    # defining arguments for parser object
+    parser.add_argument(
+        "-p", "--pdb_id", help="pdb four letter code for the protein of interest")
+    parser.add_argument(
+        "-a", "--automodel", help="the simplest method for simple comparitive modeling", action="store_true")
+    parser.add_argument(
+        "-fm", "--fixed_automodel", help="builds an automodel and keeps the non-missing residues fixed", action="store_true")
+    parser.add_argument(
+        "-l", "--loopmodel", help="builds a model by refining the loop with the missing residues", action="store_true")
+
+    #parse the arguments from standard input
+    args = parser.parse_args()
+
+    # call the main function
+    main(args.pdb_id, args.automodel, args.fixed_automodel, args.loopmodel)
